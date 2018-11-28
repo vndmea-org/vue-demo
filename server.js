f9a4c362658fc83f942467089912e200d01510f8
fanwenzhong <fanwenzhong@navercorp.com>
2018-11-28 23:47:04 +0800

    add todos.

--- server.js
+++ server.js
@@ -1,35 +1,21 @@
 const path = require('path');
 const express = require('express');
-const resolve = dir => path.join(__dirname, "..", dir);
-
 const app = express();
 const router = express.Router();
+const resolve = (dir) => path.resolve(__dirname, dir);
 
 router.use('/dist', express.static(resolve('dist')));
-router.use('/public', express.static(resolve('public')));
+app.use('/', (req, res) => {
+  if (req.url == "/") {
+    res.sendFile(path.resolve(__dirname, "./dist/index.html"));
+  } else if (/[.js|.css]$/.test(req.url)) {
+    res.sendFile(path.resolve(__dirname, `./dist${req.url}`));
+  } else {
+    res.status(404).send("Resource Not Found!");
+  }
+});
 
-function render(req, res) {
-  const s = Date.now();
-
-  res.setHeader('Content-Type', 'text/html');
-  const context = {
-    title: 'Test',
-    url: req.url,
-    headers: req.headers,
-  };
-
-  res.end(resolve('dist/index.html'));
-}
-
-app.use('/', router);
-
-router.get('*', 
-  (req, res) => {
-    render(req, res);
-  },
-);
-
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
 });
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,218 @@
 <template>
-  <div class="wrapper">{{message}}</div>
+  <div>
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input
+          class="new-todo"
+          autofocus
+          autocomplete="off"
+          placeholder="What needs to be done?"
+          v-model="newTodo"
+          @keyup.enter="addTodo"
+        >
+      </header>
+      <section class="main" v-show="todos.length" v-cloak>
+        <input class="toggle-all" type="checkbox" v-model="allDone">
+        <ul class="todo-list">
+          <li
+            v-for="todo in filteredTodos"
+            class="todo"
+            :key="todo.id"
+            :class="{ completed: todo.completed, editing: todo == editedTodo }"
+          >
+            <div class="view">
+              <input class="toggle" type="checkbox" v-model="todo.completed">
+              <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+              <button class="destroy" @click="removeTodo(todo)"></button>
+            </div>
+            <input
+              class="edit"
+              type="text"
+              v-model="todo.title"
+              v-todo-focus="todo == editedTodo"
+              @blur="doneEdit(todo)"
+              @keyup.enter="doneEdit(todo)"
+              @keyup.esc="cancelEdit(todo)"
+            >
+          </li>
+        </ul>
+      </section>
+      <footer class="footer" v-show="todos.length" v-cloak>
+        <span class="todo-count">
+          <strong>{{ remaining }}</strong>
+          {{ remaining | pluralize }} left
+        </span>
+        <ul class="filters">
+          <li>
+            <a href="#/all" :class="{ selected: visibility == 'all' }">All</a>
+          </li>
+          <li>
+            <a href="#/active" :class="{ selected: visibility == 'active' }">Active</a>
+          </li>
+          <li>
+            <a href="#/completed" :class="{ selected: visibility == 'completed' }">Completed</a>
+          </li>
+        </ul>
+        <button
+          class="clear-completed"
+          @click="removeCompleted"
+          v-show="todos.length > remaining"
+        >Clear completed</button>
+      </footer>
+    </section>
+    <footer class="info">
+      <p>Double-click to edit a todo</p>
+    </footer>
+  </div>
 </template>
+
 <script>
+const STORAGE_KEY = "todos-vuejs-2.0";
+const todoStorage = {
+  fetch: function() {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
+    todos.forEach(function(todo, index) {
+      todo.id = index;
+    });
+    todoStorage.uid = todos.length;
+    return todos;
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  }
+};
+
+const filters = {
+  all: function(todos) {
+    return todos;
+  },
+  active: function(todos) {
+    return todos.filter(function(todo) {
+      return !todo.completed;
+    });
+  },
+  completed: function(todos) {
+    return todos.filter(function(todo) {
+      return todo.completed;
+    });
+  }
+};
+
 export default {
   name: "app",
   data() {
     return {
-      message: "hello world."
+      todos: todoStorage.fetch(),
+      newTodo: "",
+      editedTodo: null,
+      visibility: "all"
     };
+  },
+
+  watch: {
+    todos: {
+      handler: function(todos) {
+        todoStorage.save(todos);
+      },
+      deep: true
+    }
+  },
+
+  computed: {
+    filteredTodos: function() {
+      return filters[this.visibility](this.todos);
+    },
+    remaining: function() {
+      return filters.active(this.todos).length;
+    },
+    allDone: {
+      get: function() {
+        return this.remaining === 0;
+      },
+      set: function(value) {
+        this.todos.forEach(function(todo) {
+          todo.completed = value;
+        });
+      }
+    }
+  },
+
+  filters: {
+    pluralize: function(n) {
+      return n === 1 ? "item" : "items";
+    }
+  },
+
+  methods: {
+    addTodo: function() {
+      const value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({
+        id: todoStorage.uid++,
+        title: value,
+        completed: false
+      });
+      this.newTodo = "";
+    },
+
+    removeTodo: function(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    },
+
+    editTodo: function(todo) {
+      this.beforeEditCache = todo.title;
+      this.editedTodo = todo;
+    },
+
+    doneEdit: function(todo) {
+      if (!this.editedTodo) {
+        return;
+      }
+      this.editedTodo = null;
+      todo.title = todo.title.trim();
+      if (!todo.title) {
+        this.removeTodo(todo);
+      }
+    },
+
+    cancelEdit: function(todo) {
+      this.editedTodo = null;
+      todo.title = this.beforeEditCache;
+    },
+
+    removeCompleted: function() {
+      this.todos = filters.active(this.todos);
+    }
+  },
+
+  directives: {
+    "todo-focus": function(el, binding) {
+      if (binding.value) {
+        el.focus();
+      }
+    }
   }
 };
+
+function onHashChange() {
+  const visibility = window.location.hash.replace(/#\/?/, "");
+  if (filters[visibility]) {
+    app.visibility = visibility;
+  } else {
+    window.location.hash = "";
+    app.visibility = "all";
+  }
+}
+
+window.addEventListener("hashchange", onHashChange);
+onHashChange();
 </script>
 
 <style lang="less" scoped>
-.wrapper {
-  background: #e2e2e2;
+[v-cloak] {
+  display: none;
 }
 </style>
