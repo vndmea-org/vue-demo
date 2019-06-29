74828600c39cf4e93dece2b41413b61d31edb99f
fanwenzhong <vndmea@gmail.com>
2019-06-30 01:47:28 +0800

    create jenkins demo branch.

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -16,9 +16,6 @@ module.exports = merge(common, {
     minimizer: [
       new UglifyJsPlugin(),
     ],
-    // splitChunks: {
-    //   chunks: 'all'
-    // }
   },
   performance: {
     maxEntrypointSize: 300000,
@@ -26,33 +23,33 @@ module.exports = merge(common, {
   },
   module: {
     rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          extractCSS: true,
-          preserveWhitespace: false,
-          postcss: [
-            autoprefixer({
-              browsers: ['last 3 versions'],
-            }),
-          ],
-        },
-      },
-      {
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader!less-loader',
-        }),
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
-      },
+      // {
+      //   test: /\.vue$/,
+      //   loader: 'vue-loader',
+      //   options: {
+      //     extractCSS: true,
+      //     preserveWhitespace: false,
+      //     postcss: [
+      //       autoprefixer({
+      //         browsers: ['last 3 versions'],
+      //       }),
+      //     ],
+      //   },
+      // },
+      // {
+      //   test: /\.less$/,
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: 'style-loader',
+      //     use: 'css-loader!less-loader',
+      //   }),
+      // },
+      // {
+      //   test: /\.css$/,
+      //   use: ExtractTextPlugin.extract({
+      //     fallback: 'style-loader',
+      //     use: 'css-loader',
+      //   }),
+      // },
     ],
   },
 });
